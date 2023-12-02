015fec232036e62949fbf0b46f86b9ca0486092b
110b13061 <113488308+110b13061@users.noreply.github.com>
2023-12-02 21:05:54 +0800

    Add files via upload

---
+++ 8-1-2-3.sql
@@ -0,0 +1,11 @@
+create table Sales
+(
+S_id CHAR(5),
+P_id CHAR(5),
+Quan INT NOT NULL,
+PRIMARY KEY(S_id,P_id),
+FOREIGN KEY(S_id)REFERENCES Employee4(Em_id)
+ON UPDATE CASCADE
+ON DELETE CASCADE,
+FOREIGN KEY(P_id)REFERENCES Product2(P_id)
+)
